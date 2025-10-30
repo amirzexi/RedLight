6365bf3c3efc4ba9714e13503b45792634704542
Amir Arz <a.arz07@outlook.com>
2025-10-30 12:19:25 +0330

    Create RedLightCalculaator method and add calculating system to RedLightCalculaator method

--- RedLight/Program.cs
+++ RedLight/Program.cs
@@ -10,6 +10,27 @@ namespace RedLight
     {
         static void Main(string[] args)
         {
+
+        }
+
+        static uint RedLightCalculaator(ushort greenLightTime , double carLong , double distanceToMove , double carsAcceleration)
+        {
+            int i = 1;
+
+            while (true)
+            {
+                double lastCarMoveDistance = carLong * i;
+                double lastCarWaitTime = i - 1;
+                double lastCarArriveTime = Math.Sqrt(((2*lastCarMoveDistance) / carsAcceleration));
+
+                double totalTime = lastCarWaitTime + lastCarArriveTime;
+
+                if(totalTime > greenLightTime)
+                {
+                    return Convert.ToUInt32(i - 1);
+                }
+                i++;
+            }
         }
     }
 }
