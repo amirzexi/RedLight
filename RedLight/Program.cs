e6860da4414546a7089fb628f5d09218a44fbba7
Amir Arz <a.arz07@outlook.com>
2025-10-30 19:28:55 +0330

    Console Write and Value reading added

--- RedLight/Program.cs
+++ RedLight/Program.cs
@@ -11,17 +11,50 @@ namespace RedLight
         static void Main(string[] args)
         {
             const string banner = @"
-  ____          _   _     _       _     _    
- |  _ \ ___  __| | | |   (_) __ _| |__ | |_  
- | |_) / _ \/ _` | | |   | |/ _` | '_ \| __| 
- |  _ <  __/ (_| | | |___| | (_| | | | | |_  
- |_| \_\___|\__,_| |_____|_|\__, |_| |_|\__| 
+     ____          _   _     _       _     _    
+    |  _ \ ___  __| | | |   (_) __ _| |__ | |_  
+    | |_) / _ \/ _` | | |   | |/ _` | '_ \| __| 
+    |  _ <  __/ (_| | | |___| | (_| | | | | |_  
+    |_| \_\___|\__,_| |_____|_|\__, |_| |_|\__| 
                             |___/      
-";
-            Console.WriteLine();
+                                   By Amir Arz";
+
+            PrintWithColor(banner, ConsoleColor.DarkRed);
+
+
+
+            
+            ushort _greenLightTime;
+            while (true)
+            {
+                try
+                {
+                    PrintWithColor("\n\n    Enter the red light time (sec) : ", ConsoleColor.Green);
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    _greenLightTime = Convert.ToUInt16(Console.ReadLine());
+                    break;
+                }
+                catch { PrintWithColor("\n    [!] Please enter valid value.",ConsoleColor.Red); }
+            }
+
+
+            PrintWithColor("\n\n    Enter the length of the car (metr) : ", ConsoleColor.Green);
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            double _carLong = Convert.ToUInt16(Console.ReadLine());
+
+
+            PrintWithColor("\n\n    Enter the distance required for each car to start moving (metr) : ", ConsoleColor.Green);
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            double _distanceToMove = Convert.ToUInt16(Console.ReadLine());
+
+
+            PrintWithColor("\n\n    Enter the acceleration of the cars (m/s^2) : ", ConsoleColor.Green);
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            double _carsAcceleration = Convert.ToUInt16(Console.ReadLine());
+
         }
 
-        static uint RedLightCalculaator(ushort greenLightTime , double carLong , double distanceToMove , double carsAcceleration)
+        static uint RedLightCalculaator(ushort greenLightTime, double carLong, double distanceToMove, double carsAcceleration)
         {
             int i = 1;
 
@@ -29,11 +62,11 @@ namespace RedLight
             {
                 double lastCarMoveDistance = carLong * i;
                 double lastCarWaitTime = i - 1;
-                double lastCarArriveTime = Math.Sqrt(((2*lastCarMoveDistance) / carsAcceleration));
+                double lastCarArriveTime = Math.Sqrt(((2 * lastCarMoveDistance) / carsAcceleration));
 
                 double totalTime = lastCarWaitTime + lastCarArriveTime;
 
-                if(totalTime > greenLightTime)
+                if (totalTime > greenLightTime)
                 {
                     return Convert.ToUInt32(i - 1);
                 }
@@ -46,7 +79,7 @@ namespace RedLight
             Console.Write(text);
         }
 
-        static void PrintWithColor(string text,ConsoleColor color)
+        static void PrintWithColor(string text, ConsoleColor color)
         {
             Console.ForegroundColor = color;
             Console.Write(text);
